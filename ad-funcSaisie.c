7a10e0e3e0686aba3ebcf550ae66223b55c4ee66
Enotian <antoine.delabuis@gmail.com>
2020-09-23 10:10:08 +0200

    ad-funcSaisie.c ad-funcSaisie.h

--- ad-funcSaisie.c
+++ ad-funcSaisie.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int getPlayersValue();
+int getPlayerValue()
 {
-	int valueA = 0;
-	int valueB = 0;
+	int objectifA = 0;
+	int objectifB = 0;
 
-	printf("Joueur A, veuillez rentrer un valeur entre 50 et 100");
-	scanf_s("%d", &valueA);
-scanf()
+	do {
+		printf("Joueur A, veuillez rentrer une valeur entre 50 et 100\n");
+		scanf_s("%d", &objectifA);
+		if (objectifA < 50 || objectifA > 100)
+			printf("Erreur\n");
+
+	} while (objectifA < 50 || objectifA > 100);
+
+	do {
+		printf("Joueur B, veuillez rentrer une valeur entre 50 et 100\n");
+		scanf_s("%d", &objectifB);
+		if (objectifB < 50 || objectifB > 100)
+			printf("Erreur\n");
+
+	} while (objectifB < 50 || objectifB > 100);
+
+	return 0;
 
 }
