4aa033890112c9d5e71c7b6116067e3f7391a1b4
Nundir <mickael.courtiade@gmail.com>
2020-09-23 09:36:09 +0200

    main.c fonctionnel

--- main.c
+++ main.c
@@ -11,20 +11,31 @@ int main()
     player1Value = getPlayerValue();
     player2Value = getPlayerValue();
 
-    while(player1 != player1Value && player2 != player2Value)
+    while(player1 >= player1Value && player2 >= player2Value)
     {
-        int playerSoldeTurn = (turn == 0 ? player1Solde : player2Solde);
-        int playerValueTurn = (turn == 0 ? player1Value : player2Value);
-
         dice = diceValue();
 
-        playerSolde += dice;
-
-        if (condEleven(playerTurn))
+        if (turn == 0) 
         {
+            player1Solde += dice;
 
+            player1Solde = condEleven(player1Solde);
+            player1Solde = condPair(player1Solde, dice);
+            player1Solde = condFive(player1Solde, dice);
+            player1Solde = condEqual(player1Solde, dice);
+        }
+        else 
+        {
+            player2Solde += dice;
+
+            player2Solde = condEleven(player2Solde);
+            player2Solde = condPair(player2Solde, dice);
+            player2Solde = condFive(player2Solde, dice);
+            player2Solde = condEqual(player2Solde, dice);
         }
 
         turn = turn == 0 ? 1 : 0;
     }
+
+    checkWinnersLosers(player1Solde, player2Solde)
 }
