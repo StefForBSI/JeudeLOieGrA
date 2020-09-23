e789ed66f25e162992978beda2d72ee2a8c0da24
Anth0ny29 <contact@ferrero-anthony.fr>
2020-09-23 09:42:00 +0200

    Modification de la fonction

--- condEgal.c
+++ condEgal.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
 
 
-if
+int Egalite(int solde_joueur1, int solde_joueur2, int dice){
+
+    if (solde_joueur1 == solde_joueur2) { // Si le solde du joueur 1 est égal au solde du joueur 2
+
+    printf("Solde joueur 1 egal solde joueur 2, soustraction de la valeur du DE pour le joueur 2. ");
+    solde_joueur2 = solde_joueur2 - dice;
+    printf("Nouveau solde : %d\n\n", solde_joueur2);
+
+    }
+
+}
