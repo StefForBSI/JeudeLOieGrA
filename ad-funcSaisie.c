8892b553ce3b4cb67da13dcbb6ecb6b87e8b149d
Enotian <antoine.delabuis@gmail.com>
2020-09-23 09:27:24 +0200

    ad-funcSaisie.c

---
+++ ad-funcSaisie.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int getPlayersValue();
+{
+	int valueA = 0;
+	int valueB = 0;
+
+	printf("Joueur A, veuillez rentrer un valeur entre 50 et 100");
+	scanf_s("%d", &valueA);
+scanf()
+
+}
