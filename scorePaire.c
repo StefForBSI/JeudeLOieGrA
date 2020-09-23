7efccd407aede5d6c9b00959aa52cc7daa07e3e9
Blandine <lemaire.blandine.pro@gmail.com>
2020-09-23 09:20:07 +0200

    Changed function name
    changed the prototype and function body

--- scorePaire.c
+++ scorePaire.c
@@ -1,15 +1,15 @@
 //input:
-//  Score : Le score du joueur que l'on souhaite verifier 
-//  ValD : Valeur du de sur ce tour
+//  solde : Le score du joueur que l'on souhaite verifier 
+//  dice : Valeur du de sur ce tour
 //output:
 //  NouveauScore : Valeur du nouveau score qu'il faudra appliquer au joueur
-int scorePaire(int Score, int ValD){
+int int condPair (int solde, int dice){
     //generation d'un score temporaire:
     int NouveauScore;
-    if(score%2 == 0){ //Si le score est pair
-        NouveauScore = Score+valD; //Score + valeur du de
+    if(solde%2 == 0){ //Si le score est pair
+        NouveauScore = solde+dice; //Score + valeur du de
     }else{
-        NouveauScore = Score; //pas de changement du score
+        NouveauScore = solde; //pas de changement du score
     }
     return NouveauScore //renvoi du nouveau score
 }
--- scorePaire.h
+++ scorePaire.h
@@ -1,6 +1,6 @@
 //input:
-//  Score : Le score du joueur que l'on souhaite verifier 
-//  ValD : Valeur du de sur ce tour
+//  solde : Le score du joueur que l'on souhaite verifier 
+//  dice : Valeur du de sur ce tour
 //output:
 //  NouveauScore : Valeur du nouveau score qui sera resortie pour la donner au joueur
-int scorePaire(int Score, int ValD);
+int condPair (int solde, int dice);
