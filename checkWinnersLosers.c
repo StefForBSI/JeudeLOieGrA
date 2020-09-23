0efe6f2479afc09a88715fb01446b4e1c066d7b9
unknown <lucasprivate35@gmail.com>
2020-09-23 09:46:28 +0100

    V1 de la fonction

--- checkWinnersLosers.c
+++ checkWinnersLosers.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 
 
-int checkWinnersLosers(soldeA, soldeB) {
+void checkWinnersLosers(soldeA, soldeB, objA, objB) {
+
 	
+
+	if (soldeA == objA) {
+
+		printf("Felicitation, joueur A gagne");
+
+	}
+
+	if (soldeB == objB) {
+		
+		printf("Felicitation, joueur B gagne");
+	}
+
+	if (soldeA > objA) {
+		printf("Joueur B gagne car joueur A a depasse son objectif");
+	}
+
+	if (soldeB > objB) {
+		printf("Joueur A gagne car joueur B a depasse son objectif");
+	}
+
 
 
 }
