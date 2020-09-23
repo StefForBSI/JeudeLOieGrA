94186e66a28cb1559b27362c483acfd4817e9a2a
unknown <Zara@LSYN98916.olympe.synetis.local>
2020-09-23 10:11:01 +0200

    add file c + file h

--- funccondEleven.c
+++ funccondEleven.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int condEleven(solde)
+int condEleven(int solde)
 {
-	int solde = 0;
+	
 	if (solde % 11 == 0)
 	{
 		printf ("Votre solde est un multiple de 11\n");
-		solde = solde+2
+		solde = solde*2;
 	}
 
-	return 0;
+	return solde;
 
 }
