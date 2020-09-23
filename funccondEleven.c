fb78c9a1ceab6bb9fb603202db2ebb15b689bc46
unknown <Zara@LSYN98916.olympe.synetis.local>
2020-09-23 09:55:21 +0200

    fonction vérif multiple de 11 fichier c

--- funccondEleven.c
+++ funccondEleven.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int condEleven(solde)
+{
+	int solde = 0;
+	if (solde % 11 == 0)
+	{
+		printf ("Votre solde est un multiple de 11\n");
+		solde = solde+2
+	}
 
+	return 0;
+
+}
