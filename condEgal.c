82cda2f5bcd42941824fae1daab8eb534c16b6de
Anth0ny29 <contact@ferrero-anthony.fr>
2020-09-23 09:46:23 +0200

    changement des variables

--- condEgal.c
+++ condEgal.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 
-int Egalite(int solde_joueur1, int solde_joueur2, int dice){
+int Egalite(int solde1, int solde2, int dice){
 
-    if (solde_joueur1 == solde_joueur2) { // Si le solde du joueur 1 est égal au solde du joueur 2
+    if (solde1 == solde2) {
 
-    printf("Solde joueur 1 egal solde joueur 2, soustraction de la valeur du DE pour le joueur 2. ");
-    solde_joueur2 = solde_joueur2 - dice;
-    printf("Nouveau solde : %d\n\n", solde_joueur2);
+        solde2 = solde2 - dice;
+
+        return solde2;
+
 
     }
 
