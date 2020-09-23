9603d14fd09dd2746ffd86e11a1bb4b597a871b1
Anth0ny29 <43807800+Anth0ny29@users.noreply.github.com>
2020-09-23 10:01:43 +0200

    Af cond egal (#12)
    Ajout de la fonction condEqual

---
+++ condEgal.c
@@ -0,0 +1,13 @@
+#include <condEgal.h>
+int Egalite(int solde1, int solde2, int dice){
+
+    if (solde1 == solde2) {
+
+        solde2 = solde2 - dice;
+
+        return solde2;
+
+
+    }
+
+}
---
+++ condEgal.h
@@ -0,0 +1,1 @@
+int Egalite(int solde1, int solde2, int dice)
